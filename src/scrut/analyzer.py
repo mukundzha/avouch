82f4cb135d5d4d2859e5006e5213bd4795be263f
Mukund <mukundzha33@gmail.com>
2026-08-03 19:17:09 +0545

    feat : update analyzer.py

--- src/scrut/analyzer.py
+++ src/scrut/analyzer.py
@@ -5,8 +5,8 @@ from scrut.rules.bare_except import analyze as analyze_bare_except
 from scrut.rules.if_else_chain import analyze as analyze_if_else_chain
 from scrut.rules.empty_except import analyze as analyze_empty_except 
 from scrut.rules.local_variables import analyze as analyze_local_variables
-from scrut.rules.return_statements import analyze as analyze_return_statements
 from scrut.rules.nested_function import analyze as analyze_nested_function
+from scrut.rules.return_statements import analyze as analyze_return_statements
 import ast
 
 BLOCK_NODES = (
@@ -115,7 +115,6 @@ def analyze_file(file_path, limits):
             issues.extend(analyze_empty_except(node, limits))
             issues.extend(analyze_if_else_chain(node, limits))
             issues.extend(analyze_local_variables(node, limits))
-            issues.extend(analyze_nested_function(node, limits))
 
             if nesting_depth > limits["max_nesting"]:
                 issues.append(
@@ -137,6 +136,7 @@ def analyze_file(file_path, limits):
             )
 
             issues.extend(analyze_duplicateb(node, limits))
+            issues.extend(analyze_nested_function(node, limits))
             issues.extend(analyze_return_statements(node, limits))
 
         elif isinstance(node, ast.ClassDef):
